3bbcf9eb879851d240d3b09a7c11965847a812d1
Bakhtiar Arifin <arifin@tamasia.co.id>
2019-06-13 14:43:17 +0700

    refactor row

--- lib/src/pages/row_page.dart
+++ lib/src/pages/row_page.dart
@@ -11,128 +11,121 @@ class RowPage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Container(
-            margin: EdgeInsets.all(16),
-            padding: EdgeInsets.all(8),
+            padding: EdgeInsets.all(16),
+            child: Text('Row'),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            height: 100,
             color: Colors.yellow,
-            child: Column(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text('Row'),
-                SizedBox(height: 8),
-                Row(
-                  children: <Widget>[
-                    Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.blue,
-                    ),
-                  ],
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.blue,
                 ),
               ],
             ),
           ),
           Container(
-            margin: EdgeInsets.all(16),
-            padding: EdgeInsets.all(8),
+            padding: EdgeInsets.all(16),
+            child: Text('Row with space between'),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            height: 100,
             color: Colors.yellow,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Text('Row with space between children'),
-                SizedBox(height: 8),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.blue,
-                    ),
-                  ],
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.blue,
                 ),
               ],
             ),
           ),
           Container(
-            margin: EdgeInsets.all(16),
-            padding: EdgeInsets.all(8),
+            padding: EdgeInsets.all(16),
+            child: Text('Row with space around'),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            height: 100,
             color: Colors.yellow,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: <Widget>[
-                Text('Row with space around children'),
-                SizedBox(height: 8),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.blue,
-                    ),
-                  ],
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.blue,
                 ),
               ],
             ),
           ),
           Container(
-            margin: EdgeInsets.all(16),
-            padding: EdgeInsets.all(8),
+            padding: EdgeInsets.all(16),
+            child: Text('Row space evenly'),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            height: 100,
             color: Colors.yellow,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Text('Row with space evenly children'),
-                SizedBox(height: 8),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      color: Colors.blue,
-                    ),
-                  ],
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: Colors.blue,
                 ),
               ],
             ),
